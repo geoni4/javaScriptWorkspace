897d7260d4d450e2327bae2ae1a1ad003f443187
geoni4 <seralove4@gmail.com>
2023-08-03 01:41:43 +0900

    javascript project done

---
+++ project-bootstrap/static/js/lotto.js
@@ -0,0 +1,42 @@
+﻿const lottoNode = document.querySelector("#lotto-container");
+
+let isLottoRunning = false;
+
+const appendNumber = async (n) => {
+  const colors = ["single-digit", "teens", "twenties", "thirties", "fourties"];
+  const div = document.createElement("div");
+  div.classList.add("lotto-num", "mx-3", colors[parseInt(n / 10) % 5]);
+  div.innerText = n;
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  if(isLottoRunning)
+    lottoNode.appendChild(div);
+};
+
+
+const lotto = async () =>{
+  isLottoRunning = true;
+  lottoNode.replaceChildren();
+  let lotto = [];
+  while(1){
+    let num = parseInt(Math.random() * 45 +1);
+    if(lotto.length >= 6) break;
+    if(lotto.indexOf(num) < 0)  
+      lotto.push(num);
+  }
+  lotto.sort((a, b) => a - b);
+
+
+  // Create a function that wraps the appendChild operation in a promise
+  // Use `async/await` to execute the loop asynchronously
+  for (const n of lotto) {
+    await appendNumber(n);
+  }
+
+  isLottoRunning = false; 
+}
+
+document.querySelector("#lottery-button").addEventListener("click", () =>{
+  if (!isLottoRunning) {
+    lotto();
+  }
+});
---
+++ project-bootstrap/static/js/rps.js
@@ -0,0 +1,61 @@
+﻿const casesBtn = document.querySelectorAll(".cases > .btn");
+let computerTimeout;
+
+const cases = ["rock", "paper", "scissors"];
+const casesKor = ["바위", "보" , "가위"];
+const judgement = ["비겼습니다.", "당신이 이겼습니다.", "당신이 졌습니다."];
+
+const yourCase = document.querySelector("#your-case");
+const computerText = document.querySelector("#computer-text");
+const judgeText = document.querySelector("#judge-text");
+const computersCase = document.querySelector("#computers-case");
+
+const rpsMain = () => {
+  Array.from(casesBtn).filter(el => el.dataset.case !== "begin-game").forEach(el => {
+    el.addEventListener("click", (e) => {
+      const you = e.target.dataset.case;
+      const yourPath = `./static/images/${you}.png`;
+
+      clearTimeout(computerTimeout);
+
+      yourCase.setAttribute("src", yourPath);
+      if (cases.includes(you)) {
+        let counter = 0;
+        computerTimeout = setTimeout(() => runComputer(you, counter, 100), 100);
+      }
+    });
+  });
+};
+
+const runComputer = (you, counter, intervalDuration) => {
+  const computer = cases[parseInt(Math.random() * 30) % 3];
+  const computersPath = `./static/images/${computer}.png`;
+  computersCase.setAttribute("src", computersPath);
+  const maxCount = 12; // 총 1.5초간 실행 (0.1초 * 9번 + 0.2초 * 3번)
+
+  if (counter === 9){
+    intervalDuration = 200;
+  }
+  if (counter === maxCount) {
+    computersCase.setAttribute("src", computersPath);
+    computerText.textContent = casesKor[cases.indexOf(computer)];
+    judgeText.textContent = judgement[((cases.indexOf(you) - cases.indexOf(computer)) % 3 + 3) % 3];
+    casesBtn.forEach(el => {
+      el.classList.toggle("d-none");
+    });
+  } else {
+    computerTimeout = setTimeout(() => runComputer(you, counter + 1, intervalDuration), intervalDuration);
+  }
+};
+
+const beginBtn = document.querySelector("#begin");
+beginBtn.addEventListener("click", () => {
+  casesBtn.forEach(el => {
+    el.classList.toggle("d-none");
+  });
+  yourCase.setAttribute("src", "./static/images/rps-image.png");
+  computerTimeout = setTimeout(() => runComputer("", 13, 100), 100);
+});
+
+
+rpsMain();
